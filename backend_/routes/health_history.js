5464cb6da7ffa9a0c83e59b714280b6c974e71f6
KamyllaJawad <abdel.jawad987@outlook.com>
2023-07-08 03:06:07 -0300

    codesmell de lavanda

--- backend_/routes/health_history.js
+++ backend_/routes/health_history.js
@@ -1,7 +1,6 @@
 const create = require("../controllers/health_history/create")
 const retrieve = require("../controllers/health_history/retrieve")
 const retrieve_by_animal = require("../controllers/health_history/retrieve_by_animal")
-// const retrieve_event_type = require("../controllers/health_history/retrieve_event_type")
 const update = require("../controllers/health_history/update")
 const delete_historic = require("../controllers/health_history/delete_historic")
 const express = require('express')
@@ -9,7 +8,7 @@ const express = require('express')
 var router = express.Router();
 
 router.post("/", create)
-router.get('/retrieve/:id', retrieve); //id do animal
+router.get('/retrieve/:id', retrieve);
 router.put('/update/:id', update);
 router.delete('/delete_historic/:id', delete_historic); 
 router.get("/getAnimal/:animal_id", retrieve_by_animal)
